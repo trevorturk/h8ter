7c7aafc600be944c2ba25aaeabbce1aa12675785
Trevor Turk <trevorturk@yahoo.com>
2008-09-06 10:06:48 -0500

    reload user on user observers, should fix problem with activation codes being wrong

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -1,10 +1,12 @@
 class UserObserver < ActiveRecord::Observer
   
   def after_create(user)
+    user.reload
     UserMailer.deliver_signup_notification(user)
   end
 
   def after_save(user)
+    user.reload
     UserMailer.deliver_activation(user) if user.recently_activated?
   end
   
