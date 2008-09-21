7bc8e561545e56556e08daa9c7abff8029cd540e
Trevor Turk <trevorturk@yahoo.com>
2008-09-21 17:50:20 -0500

    use camelize instead of capitalize in fixture validator

--- test/integration/fixture_validation_test.rb
+++ test/integration/fixture_validation_test.rb
@@ -5,7 +5,7 @@ class FixtureValidationTest < ActionController::IntegrationTest
   test "fixtures should be valid" do
     models = Fixtures.all_loaded_fixtures.keys
     models.each do |model|
-      model = model.capitalize.singularize.constantize
+      model = model.camelize.singularize.constantize
       fixtures = model.find(:all)
       fixtures.each do |fixture|
         if !fixture.valid?
