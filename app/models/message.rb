89c57e4a9f5b4a2aaa269eea68378ae23fbe9ba0
Trevor Turk <trevorturk@yahoo.com>
2008-10-05 19:27:19 -0500

    twitter?

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,9 +4,14 @@ class Message < ActiveRecord::Base
   validates_length_of :body, :maximum => 140
   attr_accessible :body
   belongs_to :user, :counter_cache => true
+  after_create :create_twitter_post
   
   def self.get(page = 1)
     paginate(:page => page, :per_page => 20, :order => 'created_at desc', :include => :user)
+  end
+  
+  def create_twitter_post
+    Twitter.post('/statuses/update.json', :query => {:status => self.body}) if TWITTER_USER && TWITTER_PASS
   end
   
   def to_s
--- app/models/twitter.rb
+++ app/models/twitter.rb
@@ -3,5 +3,3 @@ class Twitter
   base_uri 'twitter.com'
   basic_auth TWITTER_USER, TWITTER_PASS
 end
-
-# Twitter.post('/statuses/update.json', :query => {:status => "It's an HTTParty and everyone is invited!"})
