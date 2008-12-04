911d7c2be8879dac92f461d031ed7e6dd4125e7c
Trevor Turk <trevorturk@yahoo.com>
2008-12-03 23:07:17 -0600

    hating on controllers

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,9 +9,6 @@ class MessagesController < ApplicationController
   def create
     @message = current_user.messages.new(params[:message])
     if @message.save
-      if TWITTER_USER && TWITTER_PASS
-        Twitter.post('/statuses/update.json', :query => {:status => @message.user.to_s + ' hates... ' + @message.body.to_s, :source => TWITTER_SOURCE})
-      end
       redirect_to :action => "index"
     else
       flash[:error] = "There was a problem sending this message. It can't be blank or more than 115 characters."
--- app/models/message.rb
+++ app/models/message.rb
@@ -4,9 +4,17 @@ class Message < ActiveRecord::Base
   validates_length_of :body, :maximum => 115
   attr_accessible :body
   belongs_to :user, :counter_cache => true
+  after_save :post_message_to_twitter
   
   def self.get(page = 1)
     paginate(:page => page, :per_page => 20, :order => 'created_at desc', :include => :user)
+  end
+  
+  def post_message_to_twitter
+    if TWITTER_USER && TWITTER_PASS
+      Twitter.post('/statuses/update.json', 
+        :query => {:status => @message.user.to_s + ' hates... ' + @message.body.to_s, :source => TWITTER_SOURCE})
+    end
   end
   
   def to_s
