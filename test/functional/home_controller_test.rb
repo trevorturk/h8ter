e443712b2287a4a9c4f1f2da89c1c5007cc711fb
Trevor Turk <trevorturk@yahoo.com>
2008-12-21 19:09:58 -0600

    add background image (via http://typogarden.org/2008/06/08/lucid) and fix up credits in readme

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -1,12 +1,8 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  
-  test "should defer test" do
-    defer
-  end
-  
-  test "should fail" do
+    
+  test "should fail/whale" do
     get :fail
     assert_redirected_to '/500.html'
   end
