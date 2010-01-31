d414b39f34ec998984f3be7145ce20500220970e
Trevor Turk <trevorturk@gmail.com>
2010-01-31 09:42:39 -0600

    use hoptoad gem instead of plugin

--- config/environment.rb
+++ config/environment.rb
@@ -3,28 +3,29 @@ require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
   require 'yaml'
-  
+
   config.time_zone = 'UTC'
   config.i18n.default_locale = :en
   config.active_record.partial_updates = true
-  
+
   config.gem 'httparty'
-  
+  config.gem 'hoptoad_notifier'
+
   config.middleware.use "NoWWW" if RAILS_ENV == 'production'
-  
+
   CONFIG = (YAML.load_file('config/config.yml')[RAILS_ENV] rescue {}).merge(ENV) # support yaml and heroku config
-  
+
   config.action_controller.session = {
     :key => CONFIG['session_key'],
     :secret => CONFIG['session_secret'] || CONFIG['secret']
   }
-    
-  RE_LOGIN_RES = %w(admin all test help blog faq message messages login logout signup settings 
-    register home info pages page faq follow followers following followings network networks 
-    invitations invitation users user about contact status downloads api jobs tos privacy favorites 
-    hate hates updates search devices device public phones phone profile profiles account accounts 
+
+  RE_LOGIN_RES = %w(admin all test help blog faq message messages login logout signup settings
+    register home info pages page faq follow followers following followings network networks
+    invitations invitation users user about contact status downloads api jobs tos privacy favorites
+    hate hates updates search devices device public phones phone profile profiles account accounts
     notifications notification subscription subscriptions welcome welcomes fail fails everyone everybody)
-  
+
 end
 
 HoptoadNotifier.configure do |config|
